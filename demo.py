4964e623fcc888f85e9130dd0839fade32aea8a4
Miron <miron@live.com>
2021-03-26 21:23:50 +0100

    Update demo

--- demo.py
+++ demo.py
@@ -3,10 +3,13 @@ Just API demo
 '''
 
 import pprint
-from lib.devices_discovery import discover_music_cast_devices
-from lib.device_info import DeviceInfo
+from lib.devices_discovery import get_device_by_name
+from lib.device_info import DeviceInfoNetusb
 
 
-for device in discover_music_cast_devices(4):
-    device_info = DeviceInfo(device['base_url'], device['control_url'])
-    pprint.pprint(device_info.get_device_info())
+# for device in discover_music_cast_devices(4):
+#     device_info = (device['base_url'], device['control_url'])
+#     pprint.pprint(device_info.get_device_info())
+dev = get_device_by_name("Sypialnia")
+info = DeviceInfoNetusb(dev)
+pprint.pprint(info.get_preset_info())
