d42fbed3a8b79968d78d4a16dddfe127a618e21c
Miron <miron@live.com>
2021-03-26 21:23:44 +0100

    Refactor device api class

--- lib/device_info.py
+++ lib/device_info.py
@@ -22,9 +22,10 @@ class DeviceInfo:
     def ___str__(self):
         return self.base_url
 
-    def api(self, endpoint):
+    def api(self, name, endpoint):
+        path = '{name}/{endpoint}'.format(name=name, endpoint=endpoint)
         return call_api(self.base_url,
-                        endpoint, self.control_url)
+                        path, self.control_url)
 
     def get_url(self):
         return self.base_url
@@ -35,16 +36,14 @@ class DeviceInfoSystem(DeviceInfo):
     Device Info System -> set of function to get system information about single Music Cast device
     '''
 
-    __ENDPOINTS = {
-        'getDeviceInfo': 'system/getDeviceInfo',
-        'getFeatures': 'system/getFeatures'
-    }
+    def __system_api(self, endpoint):
+        return self.api('system', endpoint)
 
     def get_device_info(self):
-        return self.api(self.__ENDPOINTS['getDeviceInfo'])
+        return self.__system_api('getDeviceInfo')
 
     def get_features(self):
-        return self.api(self.__ENDPOINTS['getFeatures'])
+        return self.__system_api('getFeatures')
 
 
 class DeviceInfoNetusb(DeviceInfo):
@@ -52,13 +51,11 @@ class DeviceInfoNetusb(DeviceInfo):
     Device Info System -> set of function to get system information about single Music Cast device
     '''
 
-    __ENDPOINTS = {
-        'getPresetInfo': 'netusb/getPresetInfo',
-        'getPlayInfo': 'netusb/getPlayInfo'
-    }
+    def __system_api(self, endpoint):
+        return self.api('netusb', endpoint)
 
     def get_preset_info(self):
-        return self.api(self.__ENDPOINTS['getPresetInfo'])
+        return self.__system_api('getPresetInfo')
 
     def get_play_info(self):
-        return self.api(self.__ENDPOINTS['getPlayInfo'])
+        return self.__system_api('getPlayInfo')
