9002470a074b3d5f3c1e6da08d64998a8235e922
Miron <miron@live.com>
2021-03-26 21:15:52 +0100

    Add netusb api

--- lib/device_info.py
+++ lib/device_info.py
@@ -3,28 +3,62 @@ Device Info class
 """
 from .call_api import call_api
 
+__DEFAULT_CONTROL_URL = '/YamahaExtendedControl/v1/'
+
 
 class DeviceInfo:
     '''
-    Device info -> set of function to get information about single Music Cast device
+    Device info -> base class to get basic iformation about device
     '''
-    ENDPOINTS = {
+
+    device_addressa = ""
+
+    def __init__(self, device):
+        self.base_url = device if isinstance(
+            device, str) else device['base_url']
+        self.control_url = __DEFAULT_CONTROL_URL if isinstance(
+            device, str) else device['control_url']
+
+    def ___str__(self):
+        return self.base_url
+
+    def api(self, endpoint):
+        return call_api(self.base_url,
+                        endpoint, self.control_url)
+
+    def get_url(self):
+        return self.base_url
+
+
+class DeviceInfoSystem(DeviceInfo):
+    '''
+    Device Info System -> set of function to get system information about single Music Cast device
+    '''
+
+    __ENDPOINTS = {
         'getDeviceInfo': 'system/getDeviceInfo',
         'getFeatures': 'system/getFeatures'
     }
 
-    device_addressa = ""
-
-    def __init__(self, base_url, control_url='/YamahaExtendedControl/v1/'):
-        self.base_url = base_url
-        self.control_url = control_url
-
-    def __api(self, endpoint):
-        return call_api(self.base_url,
-                        endpoint, self.control_url)
-
     def get_device_info(self):
-        return self.__api(DeviceInfo.ENDPOINTS['getDeviceInfo'])
+        return self.api(self.__ENDPOINTS['getDeviceInfo'])
 
     def get_features(self):
-        return self.__api(DeviceInfo.ENDPOINTS['getFeatures'])
+        return self.api(self.__ENDPOINTS['getFeatures'])
+
+
+class DeviceInfoNetusb(DeviceInfo):
+    '''
+    Device Info System -> set of function to get system information about single Music Cast device
+    '''
+
+    __ENDPOINTS = {
+        'getPresetInfo': 'netusb/getPresetInfo',
+        'getPlayInfo': 'netusb/getPlayInfo'
+    }
+
+    def get_preset_info(self):
+        return self.api(self.__ENDPOINTS['getPresetInfo'])
+
+    def get_play_info(self):
+        return self.api(self.__ENDPOINTS['getPlayInfo'])
